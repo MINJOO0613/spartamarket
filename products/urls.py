c64e4f534122de53a4587f27f03cd73d4be9d037
unknown <rlaalswn0613@gmail.com>
2024-08-21 01:12:24 +0900

    feat: urls 기본 분기
    각 앱별로 urls 분기함

---
+++ products/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+app_name = "products"
+urlpatterns = [
+    # path('create/', views.create, name='create'),
+    # path('<int:pk>/', views.product_detail, name='product_detail'),
+    # path('<int:pk>/delete/', views.delete, name='delete'),
+    # path('<int:pk>/update/', views.update, name='update'),
+    # #
+    # path('index/', views.index, name='index'),
+    # #
+    # path('<int:pk>/comments', views.comment_create, name="comment_create"),
+]
--- spartamarket/urls.py
+++ spartamarket/urls.py
@@ -16,7 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include("arccounts.urls")),
+    path('products/', include('products.urls')),
+
 ]
+
+if settings.DEBUG:  # DEBUG는 개발모드라는 뜻
+    urlpatterns += static(settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT)
