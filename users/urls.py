acc2952f9d36210ca1dce3ede70f87eb4c481a85
unknown <rlaalswn0613@gmail.com>
2024-08-26 00:08:34 +0900

    feat: 팔로우, 팔로워 기능 구현 완료
    프로필페이지 일부 수정

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,7 @@ app_name = "users"
 urlpatterns = [
     path('profile/<str:username>/', views.profile_view, name='profile'),
     path('profile/<str:username>/edit/', views.edit_profile, name='edit_profile'),
+    path('profile/<str:username>/follow/', views.follow_user, name='follow_user'),
+    path('profile/<str:username>/unfollow/', views.unfollow_user, name='unfollow_user'),
+
 ]
